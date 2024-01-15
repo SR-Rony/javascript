03f78bfcf987c9b85f324330801458f6893015fd
SR Rony <146630569+SR-Rony@users.noreply.github.com>
2024-01-14 19:16:15 -0800

    javascript all event add

---
+++ event/event.js
@@ -0,0 +1,37 @@
+let button =document.querySelector('button');
+
+let heading =document.querySelector('h1');
+
+let input =document.querySelector('input')
+
+let paragraph = document.querySelector('p')
+
+//=============== onclick event ================
+
+// button.addEventListener('click',()=>{
+//     alert('hello')
+// })
+
+//==================== mouseover event=============
+// button.addEventListener('mouseover',()=>{
+//     heading.innerHTML='hello wordl'
+//     heading.style.background='red'
+//     heading.style.color='white'
+// })
+
+//==================== mouseout event=============
+// button.addEventListener('mouseout',()=>{
+//     heading.innerHTML='i am heading'
+// })
+
+//==================== change event=============
+// input.addEventListener('change',(e)=>{
+//     // paragraph.innerHTML=e.target.value
+//     // console.log(e);
+// })
+
+//==================== keydown event=============
+// input.addEventListener('keydown',(e)=>{
+//     console.log(e.target.value);
+//     paragraph.innerHTML=e.target.value
+// })
