8b29eac251cb7ee5c97ea6ed5bab28dcb92ab5c6
SR-Rony <srrony707@gmail.com>
2024-01-17 09:13:15 +0600

    all string fun add

---
+++ string/string.js
@@ -0,0 +1,29 @@
+let string = 'baangladesh'
+let lenth =string.length
+// console.log(lenth);
+let stringCharat=string.charAt()
+// console.log(stringCharat);
+let charCode =string.charCodeAt(7)
+// console.log(charCode);
+let stringAT =string
+// console.log(stringAT);
+let slice =string.slice(1,4)
+// console.log(slice);
+let subStr =string.substring(1,5)
+// console.log(subStr);
+let upper=string.toUpperCase()
+// console.log(upper);
+let lower=string.toLowerCase()
+// console.log(lower);
+let string1 = 'welcome to'
+let concat =string1.concat(" ", string)
+// console.log(concat);
+let pad =string.padStart(10,"x")
+// console.log(pad);
+let repeat =string.repeat(2)
+// console.log(repeat);
+let replace = string.replace('baangladesh','welcome')
+// console.log(replace);
+let split =string.split('')
+console.log(split);
+
