02ee8a46a143dfbf04b4d5c85cd41175c5f115cb
SR Rony <sr@sr.rony>
2024-02-17 21:02:58 +0600

    javascript object add

---
+++ objects/object.js
@@ -0,0 +1,26 @@
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+
+//   console.log(person.firstName);
+//   console.log(person.lastName);
+
+// object in function
+
+const my ={
+    name:"rony",
+    age :22,
+    fun : function(){
+        let parson = this.name+this.age
+        return parson
+    },
+    lang : ["bangla","english","hendi"]
+}
+
+// console.log(my.fun());
+console.log(my.lang);
+
+
