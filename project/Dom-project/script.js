80dacbb90355bbca143984f8097c6f1497307639
SR Rony <146630569+SR-Rony@users.noreply.github.com>
2024-06-05 10:33:14 +0600

    dom project color copy button add

--- project/Dom-project/script.js
+++ project/Dom-project/script.js
@@ -1,16 +1,21 @@
 const mainButton = document.getElementById("main-button")
 const body = document.querySelector("body")
 const input = document.querySelector("input")
+const copyButton = document.querySelector(".copy-button")
 
 
-
+// create color button
 mainButton.addEventListener("click",function () {
     let bgColor = rendomColor()
     console.log(bgColor);
     body.style.background = bgColor
     input.value = bgColor
+    copyButton.addEventListener("click",function(){
+       navigator.clipboard.writeText(input.value)
+    })
 })
 
+// color function
 function rendomColor (){
     const red = Math.floor(Math.random()*255);
     const green = Math.floor(Math.random()*255);
