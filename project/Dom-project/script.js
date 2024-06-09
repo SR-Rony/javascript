632025e5e2211059e49d77d0a95b8d39ecf4dfe7
SR Rony <146630569+SR-Rony@users.noreply.github.com>
2024-06-09 21:37:45 +0600

    copy tost messages add and remove

--- project/Dom-project/script.js
+++ project/Dom-project/script.js
@@ -3,15 +3,24 @@ const body = document.querySelector("body")
 const input = document.querySelector("input")
 const copyButton = document.querySelector(".copy-button")
 
+let div = null
 
 // create color button
 mainButton.addEventListener("click",function () {
     let bgColor = rendomColor()
-    console.log(bgColor);
     body.style.background = bgColor
+
+    // input color code add
     input.value = bgColor
+
+    // copy button
     copyButton.addEventListener("click",function(){
+        if(div != null){
+            div.remove()
+            div = null
+        }
        navigator.clipboard.writeText(input.value)
+       tostMessage(`${input.value} Copied`)
     })
 })
 
@@ -23,4 +32,21 @@ function rendomColor (){
 
     return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
     
+}
+
+// create a tost messages
+function tostMessage(msg){
+    div = document.createElement('div')
+    div.innerText = msg;
+    div.className = "tost-messages tost-in"
+
+    div.addEventListener("click",()=>{
+        div.classList.remove("tost-in");
+        div.classList.add("tost-out")
+        div.addEventListener("animationend",()=>{
+            div.remove()
+            div = null
+        })
+    })
+    document.body.appendChild(div)
 }
