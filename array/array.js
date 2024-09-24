199c926726e56d1c1c92a69f0e1376d6abd71be9
SR-Rony <srrony707@gmail.com>
2024-09-24 21:40:57 +0600

    array filter mathot add

--- array/array.js
+++ array/array.js
@@ -1,9 +1,28 @@
 // console.log('iam a array');
 
-let array = [3,4,5,6,743,3,35,7]
-console.log(array.length);
+let array = [3,4,5,6,743,3,35,7,111,333,4444]
+let num = 0
 
-array.map((item)=>{
-    console.log(item);
-    
-})
+// let result = array.map((item)=>{
+//         console.log(item);
+        
+//     })
+
+//jogfol
+// let result = array.filter((item)=>{
+//     if(num<item){
+//         return num += item
+//     }
+// })
+
+// big number
+// let result = array.filter((item)=>{
+//     if(num<item){
+//         return num = item
+//     }
+// })
+
+console.log(num);
+
+
+
