6c6cbd61039bd48805536d12a65c8846f4a9cd7c
SR-Rony <srrony707@gmail.com>
2024-09-26 22:05:30 +0600

    array mathot use

--- array/array.js
+++ array/array.js
@@ -1,7 +1,23 @@
 // console.log('iam a array');
 
-let array = [3,4,5,6,743,3,35,7,111,333,4444]
+let array = [[4,5,6,33,55,66,77],[4,5,6,7],[66,777,888,99]]
 let num = 0
+
+
+
+let arr = array.flat()
+// let arr = array.flat()
+// let arr = array.unshift(44)
+// let arr = array.shift()
+// let arr = array.push(33)
+// let arr = array.pop()
+// let arr = array.join('*')
+// let arr = array.toString()
+console.log(array);
+console.log(arr);
+
+
+
 
 // let result = array.map((item)=>{
 //         console.log(item);
@@ -22,7 +38,7 @@ let num = 0
 //     }
 // })
 
-console.log(num);
+// console.log(num);
 
 
 
